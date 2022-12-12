ee5119e7cddb6bbad625c0a4c3d414b0e66c01ac
denimwenda <denimwendwa@gmail.com>
2022-12-12 12:07:57 -0800

    bug fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - program that prints all possible different combinations of three digits
+ * main - program prints possible different combinations of three digits
  *
  * Return: 0
  */
