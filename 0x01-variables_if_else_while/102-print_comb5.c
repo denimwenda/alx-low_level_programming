afc9b45f469a311d2d577518f1c349a619fe0a6a
denimwenda <denimwendwa@gmail.com>
2022-12-12 12:57:45 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ *
+ * Description: write a programme that prints
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+	int f_d;
+	int l_d;
+
+	while (c <= 98)
+	{
+		f_d = (c / 10 + '0');
+		l_d = (c % 10 + '0');
+		c2 = 0;
+		while (c2 <= 99)
+		{
+			f_d2 = (c / 10 + '0');
+			l_d2 = (c % 10 + '0');
+
+			if (c < c2)
+			{
+				putchar(f_d);
+				putchar(l_d);
+				putchar(' ');
+				putchar(f_d2);
+				putchar(l_d2);
+
+				if (c != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+			c2++;
+		}
+		c++;
+	}
+return : 0
+}
