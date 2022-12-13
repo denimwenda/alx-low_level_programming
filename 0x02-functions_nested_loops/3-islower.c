721eeeeb507fb1c0a9cc9e6583e46730bdc49dd8
denimwenda <denimwendwa@gmail.com>
2022-12-13 01:16:33 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - checks lowwercase letters
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	if (c >= a && c <= z)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar ('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-prints
+checks
