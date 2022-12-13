e95ac5ea3c4798eac67d621e33c36c01e848b950
denimwenda <denimwendwa@gmail.com>
2022-12-13 01:36:37 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * main - checks for alphabetic characters
+ *
+ * @c: parameter
+ * Return: 1 for english character
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= a && c <= z) || (c >= A && c <= Z))
+		return (1);
+	else
+		return (0);
+}
