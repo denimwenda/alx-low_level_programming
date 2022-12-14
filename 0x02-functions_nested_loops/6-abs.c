fc9990ca39caa68d23f62744cdc9d93213f0f1c8
denimwenda <denimwendwa@gmail.com>
2022-12-14 13:45:02 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - calculate absolute value
+ * @i: parameter
+ * Return: absolute value of input
+ */
+int _abs(int i)
+{
+	if {i < 0)
+		i = i * -1;
+	return (1);
+}
