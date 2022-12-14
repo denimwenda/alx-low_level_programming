25c470d1fa9625d963001f15ebe9eafc611f50e9
denimwenda <denimwendwa@gmail.com>
2022-12-13 18:57:42 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * main - main block
+ * main - Entry point
  *
- * Description: write a programme that prints
- * Return: 0
+ *
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int c = 0;
-	int f_d;
-	int l_d;
+	int num1, num2;
 
-	while (c <= 98)
-	{
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-		c = 0;
-		while (c <= 99)
+	for (num1 = 0; num1 <= 98; num1++)
+	{	
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			f_d = (c / 10 + '0');
-			l_d = (c % 10 + '0');
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
 
-			if (c < c)
-			{
-				putchar(f_d);
-				putchar(l_d);
-				putchar(' ');
-				putchar(f_d);
-				putchar(l_d);
+			if (num1 == 98 && num2 == 99)
+				continue;
 
-				if (c != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-
-			}
-			c++;
+			putchar(',');
+			putchar(' ');
 		}
-		c++;
 	}
-return : 0
+	putchar('\n');
+
+	return (0);
 }
