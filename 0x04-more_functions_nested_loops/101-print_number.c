64759e91332672fd14c0072f32fd3193a36e1ac4
denimwenda <denimwendwa@gmail.com>
2022-12-16 19:31:38 -0800

    last

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,17 +5,20 @@
  */
 void print_number(int n)
 {
-	unsigned int i = n;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		i = -i;
-	}
-	if (i / 10)
-	{
-		print_number(i / 10);
+		n *= -1;
+		k = n;
+		putchar('-');
 	}
 
-	_putchar(i % 10 + '0');
+	k /= 10;
+
+	if (k != 0)
+	{
+		print_number(k);
+	}
+	putchar((unsigned int) n % 10 + '0');
 }
