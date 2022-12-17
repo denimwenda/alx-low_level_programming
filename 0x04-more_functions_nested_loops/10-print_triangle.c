f891fcf4cfca4e163aa5388b1dca2dc040d235e2
denimwenda <denimwendwa@gmail.com>
2022-12-16 19:08:21 -0800

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,41 +12,21 @@ void print_triangle(int size)
 	int i, j, k;
 	
 	if (size <= 0)
-
-					{
-
-								_putchar('\n');
-
-									}
-
-				else
-
-						{
-
-									for (i = 1; i <= size; i++)
-
-												{
-
-																for (k = size - i; k >= 1; k--)
-
-																				{
-
-																									_putchar(' ');
-
-																												}
-
-																			for (j = 1; j <= i; j++)
-
-																							{
-
-																												_putchar('#');
-
-																															}
-
-																						_putchar('\n');
-
-																								}
-
-										}
-
+	{
+		_putchar('\n');
+	} else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (k = size - i; k >= 1; k--)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n')
+		}
+	}
 }
