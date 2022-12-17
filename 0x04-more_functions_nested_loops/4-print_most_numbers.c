3dcd3e0f34b12d87a2ba5756d94d93c70795c356
denimwenda <denimwendwa@gmail.com>
2022-12-16 18:14:43 -0800

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,16 @@
 #include "main.h"
-
 /**
- * print_most_numbers - prints
- *
+ * print_most_numbers - prints numbers
  * Return: void
  */
-
 void print_most_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c < '10'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
-			_putchar(c);
+		if(!(c == '2' || c == '4')
+				_putchar(c);
 	}
-	_putchar('\n')
-}
+	_putchar('\n');
+	}
