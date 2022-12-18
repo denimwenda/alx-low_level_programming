0b7d63c0df8b8634751499e104780fbd55b08205
denimwenda <denimwendwa@gmail.com>
2022-12-17 23:01:06 -0800

    task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,22 +3,19 @@
  * print_number - prints an integer
  * @n:.input integer
  */
+
 void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int i = n;
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
-		putchar('-');
+		_putchar(45);
+		i = -i;
 	}
-
-	k /= 10;
-
-	if (k != 0)
+	if (i / 10)
 	{
-		print_number(k);
+		print_number(i / 10)
 	}
-	putchar((unsigned int) n % 10 + '0');
+	_putchar(i % 10 + '0');
 }
