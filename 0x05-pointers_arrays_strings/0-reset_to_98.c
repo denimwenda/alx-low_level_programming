28da8d3cacad6697fb145693c491ce149b204879
denimwenda <denimwendwa@gmail.com>
2022-12-19 11:08:19 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - resets the value of a given integer to 98
+ * reset_to_98 - takes a pointer to an int as parameter
  * @n: checked parameter
  * Return: Always 0
  */
