72529dc72ede97d5c87ba8de1a9aec340c0d8922
denimwenda <denimwendwa@gmail.com>
2022-12-19 12:50:35 -0800

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: the string
+ * Return: 0
+ */
+
+void print_rev(char *s)
+{
+	int l, k;
+
+	k = 0;
+	while (s[k] != '\0')
+		k++;
+
+	for (l = j - 1; i >= 0; i--)
+	{
+		_putchar(s[l]);
+	}
+	_putchar('\n');
+}
