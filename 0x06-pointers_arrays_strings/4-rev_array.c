7cb9d9b0e848d26c8737705135190b58adf73cc4
denimwenda <denimwendwa@gmail.com>
2022-12-21 21:16:41 -0800

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reversr_array - reverses the content of an array of integers
+ * @a:array
+ * @n:integer
+ */
+void reverse_array(int *a, int n)
+{
+	int d, f;
+
+	for (d = 0; (d < (n - 1) / 2); d++)
+	{
+		f = a[d];
+		a[d] = a[n - 1 - d];
+		a[n - 1 - d] = f;
+	}
+}
