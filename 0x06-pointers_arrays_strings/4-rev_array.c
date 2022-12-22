1d39946f0eeaae6e49b132e906f796e81d0acc9d
denimwenda <denimwendwa@gmail.com>
2022-12-21 21:19:44 -0800

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * reversr_array - reverses the content of an array of integers
+ * reverse_array - reverses the content of an array of integers
  * @a:array
  * @n:integer
  */
