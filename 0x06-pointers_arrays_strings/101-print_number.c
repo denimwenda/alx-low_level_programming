69598827066f6b77bf041ecf0781b4202f15654c
denimwenda <denimwendwa@gmail.com>
2022-12-22 17:57:08 -0800

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,17 @@
  */
 void print_number(int n)
 {
-	unsigned int p;
+	unsigned int i;
 
 	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
 	}
-	p = n;
+	i = n;
 
-	if (p / 10)
-		print_number(P / 10);
-	_putchar(p % 10 + '0');
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
 }
