bd0c1a63537ce6c9f751cf3c69b2f6046a622a45
denimwenda <denimwendwa@gmail.com>
2023-01-04 06:27:43 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in string
+ * @s: string
+ * @c: character
+ *
+ * Return: pointer for character c or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int j;
+
+	for (j = 0; *(s = j); j++)
+	{
+		if (*(s + j) == c)
+			return (s + j)
+	}
+	if (*(s + j) == c)
+		return (s + j)
+	return (0);
+}
