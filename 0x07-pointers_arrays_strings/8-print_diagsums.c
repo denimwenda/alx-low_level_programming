c5703f6f620cf3748c76ad568f1ac6ed882988f3
denimwenda <denimwendwa@gmail.com>
2023-01-04 11:37:47 -0800

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * @a: pointer to start
+ * @size: width of column
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int w, x, y, z = 0, s = 0;
+
+	for (w = 0; w < size; w++)
+	{
+		p = (w * size) + w;
+		l += *(a + y);
+	}
+	for (x = 0; x < size; x++)
+	{
+		p = (x * size) + (size - 1 - x);
+		s += *(a + y);
+	}
+	printf("%i, %i\n", z, s);
+}
