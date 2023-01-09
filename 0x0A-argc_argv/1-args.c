88d6be1d44c79ee311448d915e744dfa803b34f8
denimwenda <denimwendwa@gmail.com>
2023-01-09 12:20:32 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it
+ * @argv: int
+ * @argc: int
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
