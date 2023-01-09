1997b602029c6b62c34b5194ce6e4bee690a096d
denimwenda <denimwendwa@gmail.com>
2023-01-09 13:41:24 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdli.h>
+/**
+ * main - multiplies two numbers
+ * @argv: int
+ * @argc: int
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int v, c;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	c = atoi(argv[1]);
+	v = atoi(argc[2]);
+
+	printf("%d\n", c * v);
+	return (0);
+}
