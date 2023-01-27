bb0628cd982fe1f4c3d3e5231a30e7d4e5a65463
denimwenda <denimwendwa@gmail.com>
2023-01-26 22:14:30 -0800

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * init_dog - Initializes a dog structure
+ * @d: a dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
