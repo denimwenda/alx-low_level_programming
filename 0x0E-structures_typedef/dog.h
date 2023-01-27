9a872e30ecd86c532c2b20b5289aac82eac21047
denimwenda <denimwendwa@gmail.com>
2023-01-26 21:35:30 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,11 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG
+#define DOG
 
 /**
- * dog_t - typedef for strut dog
- *
- */
-typedef struct dog dog_t;
-
-/**
- * struct dog - new type
+ * struct dog - dog class
  * @name: name of the dog
  * @age: age of the dog
- * @owner: struct called "dog" that stores its name, its age and the name of its owner
+ * @owner: owner of the dog
  */
 struct dog
 {
@@ -19,8 +13,10 @@ struct dog
 	float *age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
