64a0ea65041dbcdd13c3eae540fc404e799d21e2
denimwenda <denimwendwa@gmail.com>
2023-02-01 13:13:43 -0800

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,14 +6,14 @@
  */
 int pop_listint(listint_t **head)
 {
-	int b;
+	int n;
 	listint_t *tmp;
 
 	if (head == NULL || *head == NULL)
 		return (0);
-	b = (*head)->b;
+	n = (*head)->n;
 	tmp = *head;
 	*head = (*head)->next;
 	free(tmp);
-	return (b);
+	return (n);
 
