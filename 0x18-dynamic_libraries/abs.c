9e6294544cca61bad1239e53bf21e2deae146448
denimwenda <denimwendwa@gmail.com>
2023-03-20 13:43:09 -0700

    changes

--- 0x18-dynamic_libraries/abs.c
+++ 0x18-dynamic_libraries/abs.c
@@ -1,9 +1,28 @@
-#include<stdio.h>
+#include "main.h"
+#include <stdio.h>
+
 /**
- * _abs - gives absolute value
+ * _abs - value absolute
  *
+ * @n: The int to print
+ * Return: Always 0.
  */
+
 int _abs(int n)
 {
-	return (0);
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+
+	else if (n == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (n);
+	}
+
 }
---
+++ 0x18-dynamic_libraries/atoi.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _atoi - changes string to int
+ * @s: char
+ */
+int _atoi(char *s)
+{
+	return (0);
+}
